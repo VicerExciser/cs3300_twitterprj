ccb9e77790264589df79424896682959314994f3
Austin Condict <acondict3@gatech.edu>
2018-05-17 13:56:54 -0400

    first commit

---
+++ twitterprj/config.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#       https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "QXj54kXJInr19r1JjTYN4sC0k"
+consumer_secret = "GZ4vuGJInRvcxJms33Wu7K4vsrHYud7L4fcOHjidA3EQ6WnFsm"
+access_key = "997165307251703808-wxIufckV9FES2QQmSlhz3h4FfnCs7zZ"
+access_secret = "JFxG9Z6zBfWa0fZZmbMlvdNg4RgDApFjVhPgkod6oPfhq"
+
